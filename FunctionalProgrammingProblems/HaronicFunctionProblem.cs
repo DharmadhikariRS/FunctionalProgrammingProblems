42e6073104c90422de10a8cce97b80a9894b9b4b
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-11 17:48:40 +0530

    [Rushikesh] UC4- HArmonic Function

---
+++ FunctionalProgrammingProblems/HaronicFunctionProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    internal class HaronicFunctionProblem
+    {
+        public void Harmonicfunction()
+        {
+            float ResultHarmonic = 0;
+
+            Console.WriteLine("Enter the number= ");
+            int N = int.Parse(Console.ReadLine());
+            while (N < 0)
+            {
+                Console.WriteLine("Enter the number greater than 0 ");
+                N = int.Parse(Console.ReadLine());
+            }
+
+
+            for (float i = 1; i <= N; i++)
+            {
+                ResultHarmonic += 1 / i;
+            }
+            Console.WriteLine("Harmonic result is= " + ResultHarmonic);
+        }
+    }
+}
--- FunctionalProgrammingProblems/Program.cs
+++ FunctionalProgrammingProblems/Program.cs
@@ -6,7 +6,9 @@
         {
             Console.WriteLine("Choose your selection from below");
             Console.WriteLine("1. Flip Coin Percentage prorram");
+            Console.WriteLine("2 Leap year ");
             Console.WriteLine("3. Power of Two");
+            Console.WriteLine("4. Harmonic Number");
             int n = Convert.ToInt32(Console.ReadLine());
             switch (n)
             {
@@ -23,6 +25,11 @@
                 case 3:
                     PowersOfTwo powersOfTwo=new PowersOfTwo();
                     powersOfTwo.PowerOfTwo();
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    HaronicFunctionProblem haronicFunctionProblem= new HaronicFunctionProblem();
+                    haronicFunctionProblem.Harmonicfunction();
                     Console.WriteLine();
                     break;
                 default:
