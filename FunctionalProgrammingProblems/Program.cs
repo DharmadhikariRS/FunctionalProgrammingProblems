5c4f2171b8c2ca7401068d77fff765e44f2de3a6
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-11 17:07:01 +0530

    [Rushikesh] UC1- Flip coin percentage

---
+++ FunctionalProgrammingProblems/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FunctionalProgrammingProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose your selection from below");
+            Console.WriteLine("1. Flip Coin Percentage prorram");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    FlipCoinPercentProgram flipCoinPercentProgram = new FlipCoinPercentProgram();
+                    flipCoinPercentProgram.FlipCoinPercentage();
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Make Proper Selection");
+                    break;
+            }
+        }
+    }
+}
